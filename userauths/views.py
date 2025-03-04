6776770b7124f8736a0b3741ec82d4963be925ab
Bilal ŞAHİN <bilalsahin37@hotmail.com>
2025-03-04 23:33:10 +0300

    Add RegisterView to handle user registration form submission

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from forms import UserRegisterForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.messages import success
 
-# Create your views here.
+def RegisterView(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'userauths/registration.html', {'form': form})
+    else:
+        form = UserRegisterForm()
+    return render(request, 'userauths/registration.html', {'form': form})
