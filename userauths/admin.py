b4038bd332f4328f7a9707a1070dc84e890b7478
Bilal ŞAHİN <bilalsahin37@hotmail.com>
2025-03-04 20:35:09 +0300

    Enhance admin interface for User and Profile models with custom display and edit options

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,6 +1,17 @@
-from profile import Profile
 from django.contrib import admin
 from userauths.models import User, Profile
 
-admin.site.register(User)
-admin.site.register(Profile)
+
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "username", "email", "phone", "gender"]
+    list_editable = [ "username", "email", "phone", "gender"]
+    
+admin.site.register(User, UserCustomAdmin)
+
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["full_name", "user", "verified", "slug"]
+    list_editable = ["verified"]
+
+admin.site.register(Profile, ProfileAdmin)
