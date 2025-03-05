a462079f2e2b2024dd4882f0537ef828ca963b0d
Bilal ŞAHİN <bilalsahin37@hotmail.com>
2025-03-05 18:24:21 +0300

    Add user registration functionality with custom fields and update URL routing

--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from forms import UserRegisterForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.contrib.messages import success
+from django.shortcuts import render, redirect
+
 from django.contrib import messages
+
+from.forms import UserRegisterForm
+
+from.models import Profile
+from django.contrib.auth import authenticate, login
+
 
 def RegisterView(request):
     if request.user.is_authenticated:
@@ -11,7 +14,28 @@ def RegisterView(request):
         return render("core:feed")
 
     form = UserRegisterForm(request.POST or None)
-    
-    
+    if form.is_valid():
+        form.save()
+        full_name = form.cleaned_data.get("full_name")
+        phone = form.cleaned_data.get("phone")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        
+
+        user = authenticate(email=email, password=password)
+        login(request, user)
+
+        profile = Profile.objects.get(user.request.user)
+        profile.full_name = full_name
+        profile.phone = phone
+        profile.save()
+        
+        messages.success(request, f"You have successfully registered, {full_name}! You can now log in.")
+        return render("core:feed")
+
+    context = {
+        "form": form
+        }
+    return render(request, "userauths/sign-up.html", context)
 
    
