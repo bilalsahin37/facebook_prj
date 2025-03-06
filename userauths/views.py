d8f82ac49fd4b0dccf075cf8e1f77afb97778c50
Bilal ŞAHİN <bilalsahin37@hotmail.com>
2025-03-06 13:06:28 +0300

    Implement login and logout functionality with user authentication and enhance sign-up form structure

--- userauths/views.py
+++ userauths/views.py
@@ -1,9 +1,16 @@
+from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-from userauths.forms import UserRegisterForm
-from .models import Profile
+from userauths.models import User, Profile
+from userauths.forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+# from core.models import FriendRequest, Post
+
+
 
 
 def RegisterView(request):
@@ -42,3 +49,43 @@ def RegisterView(request):
 
     context = {"form": form}
     return render(request, "userauths/sign-up.html", context)
+
+
+
+
+
+
+def LoginView(request):
+    # if request.user.is_authenticated:
+    #     return redirect('core:feed')
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You are Logged In")
+                return redirect("core:feed")
+            else:
+                messages.error(request, "Username or password does not exit.")
+
+        except:
+            messages.error(request, "User does not exist")
+
+    return HttpResponseRedirect("/")
+
+
+
+
+
+
+def LogoutView(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect("userauths:sign-in")
